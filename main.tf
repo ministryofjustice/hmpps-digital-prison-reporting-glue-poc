b8c96357cdcff7db812b815824c4f069306e8f0b
Hari Chintala <h.chintala@isaggio.com>
2022-09-29 17:06:55 +0100

    Fixing main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket_object" "object1" {
+resource "aws_s3_bucket_object" "sample_pycode" {
     count       = var.sync_objects ? 1 : 0
 
     for_each    = fileset("src/", "*.py")
-    bucket      = arn:aws:s3:::dpr-glue-jobs-development-20220916083016134900000005
+    bucket      = "arn:aws:s3:::dpr-glue-jobs-development-20220916083016134900000005"
     key         = "${var.project}/${each.value}"
     source      = "src/${each.value}"
     etag        = filemd5("src/${each.value}")
