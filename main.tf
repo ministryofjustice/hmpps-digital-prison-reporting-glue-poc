54df740746005a83137a52d34edf1cadcfd307d0
Hari Chintala <h.chintala@isaggio.com>
2022-09-29 17:40:36 +0100

    Set Object S3 Bucket to Name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket_object" "sample_pycode" {
     for_each    = fileset("src/", "*.py")
-    bucket      = "arn:aws:s3:::dpr-glue-jobs-development-20220916083016134900000005"
+    bucket      = "dpr-glue-jobs-development-20220916083016134900000005" 
     key         = "${var.project}/${each.value}"
     source      = "src/${each.value}"
     etag        = filemd5("src/${each.value}")
